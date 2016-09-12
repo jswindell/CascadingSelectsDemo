1421ecdb4a50108873dfc6d7c115b63d318b38c1
stashuk <stashuk@yahoo.com>
2016-09-12 15:27:15 -0400

    Added GameSystemController

---
+++ CascadingSelectsDemo/Controllers/GameSystemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CascadingSelectsDemo.Controllers
+{
+    public class GameSystemController : Controller
+    {
+        // GET: GameSystem
+        public ActionResult Games(string console)
+        {
+            if (!String.IsNullOrEmpty(console) && console.Equals("Playstation 3", StringComparison.OrdinalIgnoreCase))
+            {
+                return Json(new[] { "Fifa", "Madden", "Mario" }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new[] { "" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
